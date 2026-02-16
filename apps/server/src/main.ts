f6b45b8c09485a5830894da862f6f76cfad50177
Mmoises05 <moisesrengifo98@gmail.com>
2026-02-16 10:17:56 -0500

    Fix: Add crash logging and defensive build

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -2,16 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  console.log('Starting NestJS application...');
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*', // Allow all origins for simplicity in this MVP
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: false,
-  });
-  const port = process.env.PORT ?? 4000;
-  console.log(`Listening on port: ${port}`);
-  await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  try {
+    console.log('Starting NestJS application...');
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: '*', // Allow all origins for simplicity in this MVP
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: false,
+    });
+    const port = process.env.PORT ?? 4000;
+    console.log(`Listening on port: ${port}`);
+    await app.listen(port, '0.0.0.0');
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  } catch (error) {
+    console.error('Error starting application:', error);
+    process.exit(1);
+  }
 }
 bootstrap();
